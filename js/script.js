265e53c6083e52578a5aa36d00ca873edc3e8d69
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-03-07 01:28:08 +0000

    made area and perimeter program | added index.html, script.js, + style.css

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Julianne Leblanc-Peltier All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julianne Leblanc-Peltier
+// Created on: March 2024
 // This file contains the JS functions for index.html
+
+let length = 5
+let width = 3
+const area = length*width
+const perimeter = 2*(length+width)
+
+function myButtonClicked() {
+  document.getElementById("area-of-rectangle").innerHTML = area + "cm"
+  document.getElementById("perimeter-of-rectangle").innerHTML = perimeter + "cm"
+}
